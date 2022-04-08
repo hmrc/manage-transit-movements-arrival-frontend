c5263d3f24a13754baf4a2efb8e2c0a54b73e551
carrielies <carrielies@users.noreply.github.com>
2022-04-08 16:18:43 +0100

    CTCP-130 Migrate IsTranshipmentView to Twirl

--- app/controllers/events/IsTranshipmentController.scala
+++ app/controllers/events/IsTranshipmentController.scala
@@ -23,67 +23,42 @@ import models.{Index, Mode, MovementReferenceNumber}
 import navigation.Navigator
 import pages.events.IsTranshipmentPage
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
-import renderer.Renderer
 import repositories.SessionRepository
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import uk.gov.hmrc.viewmodels.{NunjucksSupport, Radios}
+import views.html.events.IsTranshipmentView
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class IsTranshipmentController @Inject() (override val messagesApi: MessagesApi,
                                           sessionRepository: SessionRepository,
                                           navigator: Navigator,
-                                          identify: IdentifierAction,
-                                          getData: DataRetrievalActionProvider,
-                                          requireData: DataRequiredAction,
+                                          actions: Actions,
                                           formProvider: IsTranshipmentFormProvider,
                                           val controllerComponents: MessagesControllerComponents,
-                                          renderer: Renderer
+                                          view: IsTranshipmentView
 )(implicit ec: ExecutionContext)
     extends FrontendBaseController
-    with I18nSupport
-    with NunjucksSupport {
+    with I18nSupport {
 
   private val form = formProvider()
 
-  def onPageLoad(mrn: MovementReferenceNumber, eventIndex: Index, mode: Mode): Action[AnyContent] =
-    (identify andThen getData(mrn) andThen requireData).async {
-      implicit request =>
-        val preparedForm = request.userAnswers.get(IsTranshipmentPage(eventIndex)) match {
-          case None        => form
-          case Some(value) => form.fill(value)
-        }
+  def onPageLoad(mrn: MovementReferenceNumber, eventIndex: Index, mode: Mode): Action[AnyContent] = actions.requireData(mrn) {
+    implicit request =>
+      val preparedForm = request.userAnswers.get(IsTranshipmentPage(eventIndex)) match {
+        case None        => form
+        case Some(value) => form.fill(value)
+      }
 
-        val json = Json.obj(
-          "form"        -> preparedForm,
-          "mode"        -> mode,
-          "mrn"         -> mrn,
-          "radios"      -> Radios.yesNo(preparedForm("value")),
-          "onSubmitUrl" -> routes.IsTranshipmentController.onSubmit(mrn, eventIndex, mode).url
-        )
+      Ok(view(preparedForm, mrn, eventIndex, mode))
+  }
 
-        renderer.render("events/isTranshipment.njk", json).map(Ok(_))
-    }
-
-  def onSubmit(mrn: MovementReferenceNumber, eventIndex: Index, mode: Mode): Action[AnyContent] = (identify andThen getData(mrn) andThen requireData).async {
+  def onSubmit(mrn: MovementReferenceNumber, eventIndex: Index, mode: Mode): Action[AnyContent] = actions.requireData(mrn).async {
     implicit request =>
       form
         .bindFromRequest()
         .fold(
-          formWithErrors => {
-
-            val json = Json.obj(
-              "form"        -> formWithErrors,
-              "mode"        -> mode,
-              "mrn"         -> mrn,
-              "radios"      -> Radios.yesNo(formWithErrors("value")),
-              "onSubmitUrl" -> routes.IsTranshipmentController.onSubmit(mrn, eventIndex, mode).url
-            )
-
-            renderer.render("events/isTranshipment.njk", json).map(BadRequest(_))
-          },
+          formWithErrors => Future.successful(BadRequest(view(formWithErrors, mrn, eventIndex, mode))),
           value =>
             for {
               updatedAnswers <- Future.fromTry(request.userAnswers.set(IsTranshipmentPage(eventIndex), value))
