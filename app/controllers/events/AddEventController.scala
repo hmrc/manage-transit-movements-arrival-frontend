700ecffa397baced862f32a3bea8597ee82a2979
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-04-08 13:53:07 +0100

    CTCP-129-add-to-pages: Committing what I have so far.

--- app/controllers/events/AddEventController.scala
+++ app/controllers/events/AddEventController.scala
@@ -24,49 +24,47 @@ import models.requests.DataRequest
 import models.{Index, Mode, MovementReferenceNumber, UserAnswers}
 import navigation.Navigator
 import pages.events.AddEventPage
-import play.api.data.Form
 import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.libs.json.Json
 import play.api.mvc._
-import renderer.Renderer
+import uk.gov.hmrc.hmrcfrontend.views.viewmodels.addtoalist.ListItem
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import uk.gov.hmrc.viewmodels.SummaryList.Row
-import uk.gov.hmrc.viewmodels.{NunjucksSupport, Radios}
+import uk.gov.hmrc.viewmodels.NunjucksSupport
 import utils.AddEventsHelper
-import javax.inject.Inject
+import views.html.events.AddEventView
 
+import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
-class AddEventController @Inject() (override val messagesApi: MessagesApi,
-                                    navigator: Navigator,
-                                    identify: IdentifierAction,
-                                    getData: DataRetrievalActionProvider,
-                                    requireData: DataRequiredAction,
-                                    formProvider: AddEventFormProvider,
-                                    val controllerComponents: MessagesControllerComponents,
-                                    renderer: Renderer,
-                                    config: FrontendAppConfig
+class AddEventController @Inject() (
+  override val messagesApi: MessagesApi,
+  navigator: Navigator,
+  actions: Actions,
+  formProvider: AddEventFormProvider,
+  val controllerComponents: MessagesControllerComponents,
+  config: FrontendAppConfig,
+  view: AddEventView
 )(implicit ec: ExecutionContext)
     extends FrontendBaseController
     with I18nSupport
     with NunjucksSupport {
 
-  def onPageLoad(mrn: MovementReferenceNumber, mode: Mode): Action[AnyContent] = (identify andThen getData(mrn) andThen requireData).async {
+  def onPageLoad(mrn: MovementReferenceNumber, mode: Mode): Action[AnyContent] = actions.requireData(mrn) {
     implicit request =>
       val form = formProvider(allowMoreEvents(request.userAnswers))
       val preparedForm = request.userAnswers.get(AddEventPage) match {
         case None        => form
         case Some(value) => form.fill(value)
       }
-      renderView(mrn, mode, preparedForm, Results.Ok)
+
+      Ok(view(preparedForm, mrn, mode, events(mode)))
   }
 
-  def onSubmit(mrn: MovementReferenceNumber, mode: Mode): Action[AnyContent] = (identify andThen getData(mrn) andThen requireData).async {
+  def onSubmit(mrn: MovementReferenceNumber, mode: Mode): Action[AnyContent] = actions.requireData(mrn).async {
     implicit request =>
       formProvider(allowMoreEvents(request.userAnswers))
         .bindFromRequest()
         .fold(
-          formWithErrors => renderView(mrn, mode, formWithErrors, Results.BadRequest),
+          formWithErrors => Future.successful(BadRequest(view(formWithErrors, mrn, mode, events(mode)))),
           value =>
             for {
               updatedAnswers <- Future.fromTry(request.userAnswers.set(AddEventPage, value))
@@ -74,30 +72,12 @@ class AddEventController @Inject() (override val messagesApi: MessagesApi,
         )
   }
 
-  private def renderView(mrn: MovementReferenceNumber, mode: Mode, form: Form[Boolean], status: Results.Status)(implicit
-    request: DataRequest[AnyContent]
-  ): Future[Result] = {
-
-    val numberOfEvents = request.userAnswers.get(DeriveNumberOfEvents).getOrElse(0)
-
-    val cyaHelper            = new AddEventsHelper(request.userAnswers, mode)
-    val listOfEvents         = List.range(0, numberOfEvents).map(Index(_))
-    val eventsRows: Seq[Row] = listOfEvents.flatMap(cyaHelper.listOfEvent)
-
-    val singularOrPlural = if (numberOfEvents == 1) "singular" else "plural"
-
-    val json = Json.obj(
-      "form"            -> form,
-      "mode"            -> mode,
-      "mrn"             -> mrn,
-      "radios"          -> Radios.yesNo(form("value")),
-      "titleOfPage"     -> msg"addEvent.title.$singularOrPlural".withArgs(numberOfEvents),
-      "heading"         -> msg"addEvent.heading.$singularOrPlural".withArgs(numberOfEvents),
-      "allowMoreEvents" -> allowMoreEvents(request.userAnswers),
-      "events"          -> eventsRows
-    )
-
-    renderer.render("events/addEvent.njk", json).map(status(_))
+  private def events(mode: Mode)(implicit request: DataRequest[_]): Seq[ListItem] = {
+    val numberOfEvents  = request.userAnswers.get(DeriveNumberOfEvents).getOrElse(0)
+    val addEventsHelper = new AddEventsHelper(request.userAnswers, mode)
+    (0 to numberOfEvents) flatMap {
+      x => addEventsHelper.listOfEvent(Index(x))
+    }
   }
 
   private def allowMoreEvents(ua: UserAnswers): Boolean =
