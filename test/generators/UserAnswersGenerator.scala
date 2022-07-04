a164cbee4c5182b037ad04f01483361ab557f19b
carrielies <carrielies@users.noreply.github.com>
2022-07-04 16:48:49 +0100

    CTCP-695 Remove old it:test

--- test/generators/UserAnswersGenerator.scala
+++ test/generators/UserAnswersGenerator.scala
@@ -27,7 +27,7 @@ trait UserAnswersGenerator extends TryValues {
   self: Generators =>
 
   val generators: Seq[Gen[(QuestionPage[_], JsValue)]] =
-      Nil
+    Nil
 
   implicit lazy val arbitraryUserData: Arbitrary[UserAnswers] = {
 
