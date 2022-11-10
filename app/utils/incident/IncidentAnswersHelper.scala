e786cb96856ac2d310c37560ddf9881776a730c2
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2022-11-10 15:04:44 +0000

    CTCP-CYA-HELPERS: Start building incident helpers

---
+++ app/utils/incident/IncidentAnswersHelper.scala
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils.incident
+
+import models.incident.IncidentCode
+import models.reference.Country
+import models.{Index, Mode, UserAnswers}
+import pages.incident._
+import play.api.i18n.Messages
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+import utils.AnswersHelper
+
+import java.time.LocalDate
+
+class IncidentAnswersHelper(
+  userAnswers: UserAnswers,
+  mode: Mode,
+  index: Index
+)(implicit messages: Messages)
+    extends AnswersHelper(userAnswers, mode) {
+
+  def country: Option[SummaryListRow] = getAnswerAndBuildRow[Country](
+    page = IncidentCountryPage(index),
+    formatAnswer = formatAsText,
+    prefix = "incident.incidentCountry",
+    id = Some("change-country")
+  )
+
+  def code: Option[SummaryListRow] = getAnswerAndBuildRow[IncidentCode](
+    page = IncidentCodePage(index),
+    formatAnswer = formatEnumAsText(IncidentCode.messageKeyPrefix),
+    prefix = "incident.incidentCode",
+    id = Some("change-code")
+  )
+
+  def text: Option[SummaryListRow] = getAnswerAndBuildRow[String](
+    page = IncidentTextPage(index),
+    formatAnswer = formatAsText,
+    prefix = "incident.incidentText",
+    id = Some("change-text")
+  )
+
+  def endorsementYesNo: Option[SummaryListRow] = getAnswerAndBuildRow[Boolean](
+    page = AddEndorsementPage(index),
+    formatAnswer = formatAsYesOrNo,
+    prefix = "incident.addEndorsement",
+    id = Some("change-add-endorsement")
+  )
+
+  def endorsementDate: Option[SummaryListRow] = getAnswerAndBuildRow[LocalDate](
+    page = EndorsementDatePage(index),
+    formatAnswer = formatAsDate,
+    prefix = "incident.endorsementDate",
+    id = Some("change-endorsement-date")
+  )
+
+  def endorsementAuthority: Option[SummaryListRow] = getAnswerAndBuildRow[String](
+    page = EndorsementAuthorityPage(index),
+    formatAnswer = formatAsText,
+    prefix = "incident.endorsementAuthority",
+    id = Some("change-endorsement-authority")
+  )
+
+  def endorsementCountry: Option[SummaryListRow] = getAnswerAndBuildRow[Country](
+    page = EndorsementCountryPage(index),
+    formatAnswer = formatAsText,
+    prefix = "incident.endorsementCountry",
+    id = Some("change-endorsement-country")
+  )
+
+  def endorsementLocation: Option[SummaryListRow] = getAnswerAndBuildRow[String](
+    page = EndorsementLocationPage(index),
+    formatAnswer = formatAsText,
+    prefix = "incident.endorsementLocation",
+    id = Some("change-endorsement-location")
+  )
+}
+
+object IncidentAnswersHelper {
+
+  def apply(
+    userAnswers: UserAnswers,
+    mode: Mode,
+    index: Index
+  )(implicit messages: Messages) =
+    new IncidentAnswersHelper(userAnswers, mode, index)
+}
---
+++ test/utils/incident/IncidentAnswersHelperSpec.scala
@@ -0,0 +1,320 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package utils.incident
+
+import base.SpecBase
+import controllers.incident.routes
+import generators.Generators
+import models.Mode
+import models.incident.IncidentCode
+import models.reference.Country
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import pages.incident._
+
+import java.time.LocalDate
+
+class IncidentAnswersHelperSpec extends SpecBase with ScalaCheckPropertyChecks with Generators {
+
+  "CheckIncidentAnswersHelper" - {
+
+    "country" - {
+      "must return None" - {
+        "when IncidentCountryPage is undefined" in {
+          forAll(arbitrary[Mode]) {
+            mode =>
+              val helper = new IncidentAnswersHelper(emptyUserAnswers, mode, incidentIndex)
+              val result = helper.country
+              result mustBe None
+          }
+        }
+      }
+
+      "must return Some(Row)" - {
+        "when IncidentCountryPage defined" in {
+          forAll(arbitrary[Country], arbitrary[Mode]) {
+            (country, mode) =>
+              val answers = emptyUserAnswers.setValue(IncidentCountryPage(incidentIndex), country)
+
+              val helper = new IncidentAnswersHelper(answers, mode, incidentIndex)
+              val result = helper.country.get
+
+              result.key.value mustBe "Country"
+              result.value.value mustBe country.toString
+              val actions = result.actions.get.items
+              actions.size mustBe 1
+              val action = actions.head
+              action.content.value mustBe "Change"
+              action.href mustBe routes.IncidentCountryController.onPageLoad(answers.mrn, mode, incidentIndex).url
+              action.visuallyHiddenText.get mustBe "incident country"
+              action.id mustBe "change-country"
+          }
+        }
+      }
+    }
+
+    "code" - {
+      "must return None" - {
+        "when IncidentCodePage is undefined" in {
+          forAll(arbitrary[Mode]) {
+            mode =>
+              val helper = new IncidentAnswersHelper(emptyUserAnswers, mode, incidentIndex)
+              val result = helper.code
+              result mustBe None
+          }
+        }
+      }
+
+      "must return Some(Row)" - {
+        "when IncidentCodePage defined" in {
+          forAll(arbitrary[IncidentCode], arbitrary[Mode]) {
+            (code, mode) =>
+              val answers = emptyUserAnswers.setValue(IncidentCodePage(incidentIndex), code)
+
+              val helper = new IncidentAnswersHelper(answers, mode, incidentIndex)
+              val result = helper.code.get
+
+              result.key.value mustBe "Incident code"
+              val key = s"incident.incidentCode.$code"
+              messages.isDefinedAt(key) mustBe true
+              result.value.value mustBe messages(key)
+              val actions = result.actions.get.items
+              actions.size mustBe 1
+              val action = actions.head
+              action.content.value mustBe "Change"
+              action.href mustBe routes.IncidentCodeController.onPageLoad(answers.mrn, mode, incidentIndex).url
+              action.visuallyHiddenText.get mustBe "incident code"
+              action.id mustBe "change-code"
+          }
+        }
+      }
+    }
+
+    "text" - {
+      "must return None" - {
+        "when IncidentTextPage is undefined" in {
+          forAll(arbitrary[Mode]) {
+            mode =>
+              val helper = new IncidentAnswersHelper(emptyUserAnswers, mode, incidentIndex)
+              val result = helper.text
+              result mustBe None
+          }
+        }
+      }
+
+      "must return Some(Row)" - {
+        "when IncidentTextPage defined" in {
+          forAll(Gen.alphaNumStr, arbitrary[Mode]) {
+            (text, mode) =>
+              val answers = emptyUserAnswers.setValue(IncidentTextPage(incidentIndex), text)
+
+              val helper = new IncidentAnswersHelper(answers, mode, incidentIndex)
+              val result = helper.text.get
+
+              result.key.value mustBe "Description"
+              result.value.value mustBe text
+              val actions = result.actions.get.items
+              actions.size mustBe 1
+              val action = actions.head
+              action.content.value mustBe "Change"
+              action.href mustBe routes.IncidentTextController.onPageLoad(answers.mrn, mode, incidentIndex).url
+              action.visuallyHiddenText.get mustBe "incident description"
+              action.id mustBe "change-text"
+          }
+        }
+      }
+    }
+
+    "endorsement-yes-no" - {
+      "must return None" - {
+        "when AddEndorsementPage is undefined" in {
+          forAll(arbitrary[Mode]) {
+            mode =>
+              val helper = new IncidentAnswersHelper(emptyUserAnswers, mode, incidentIndex)
+              val result = helper.endorsementYesNo
+              result mustBe None
+          }
+        }
+      }
+
+      "must return Some(Row)" - {
+        "when AddEndorsementPage defined" in {
+          forAll(arbitrary[Mode]) {
+            mode =>
+              val answers = emptyUserAnswers.setValue(AddEndorsementPage(incidentIndex), true)
+
+              val helper = new IncidentAnswersHelper(answers, mode, incidentIndex)
+              val result = helper.endorsementYesNo.get
+
+              result.key.value mustBe "Do you need to add an endorsement for the incident?"
+              result.value.value mustBe "Yes"
+              val actions = result.actions.get.items
+              actions.size mustBe 1
+              val action = actions.head
+              action.content.value mustBe "Change"
+              action.href mustBe routes.AddEndorsementController.onPageLoad(answers.mrn, mode, incidentIndex).url
+              action.visuallyHiddenText.get mustBe "if you need to add an endorsement for the incident"
+              action.id mustBe "change-add-endorsement"
+          }
+        }
+      }
+    }
+
+    "endorsement-date" - {
+      "must return None" - {
+        "when EndorsementDatePage is undefined" in {
+          forAll(arbitrary[Mode]) {
+            mode =>
+              val helper = new IncidentAnswersHelper(emptyUserAnswers, mode, incidentIndex)
+              val result = helper.endorsementDate
+              result mustBe None
+          }
+        }
+      }
+
+      "must return Some(Row)" - {
+        "when EndorsementDatePage defined" in {
+          forAll(arbitrary[Mode]) {
+            mode =>
+              val date    = LocalDate.of(2021, 9, 9)
+              val answers = emptyUserAnswers.setValue(EndorsementDatePage(incidentIndex), date)
+
+              val helper = new IncidentAnswersHelper(answers, mode, incidentIndex)
+              val result = helper.endorsementDate.get
+
+              result.key.value mustBe "Endorsement date"
+              result.value.value mustBe "9 September 2021"
+              val actions = result.actions.get.items
+              actions.size mustBe 1
+              val action = actions.head
+              action.content.value mustBe "Change"
+              action.href mustBe routes.EndorsementDateController.onPageLoad(answers.mrn, incidentIndex, mode).url
+              action.visuallyHiddenText.get mustBe "endorsement date"
+              action.id mustBe "change-endorsement-date"
+          }
+        }
+      }
+    }
+
+    "endorsement-authority" - {
+      "must return None" - {
+        "when EndorsementAuthorityPage is undefined" in {
+          forAll(arbitrary[Mode]) {
+            mode =>
+              val helper = new IncidentAnswersHelper(emptyUserAnswers, mode, incidentIndex)
+              val result = helper.endorsementAuthority
+              result mustBe None
+          }
+        }
+      }
+
+      "must return Some(Row)" - {
+        "when EndorsementDatePage defined" in {
+          forAll(Gen.alphaNumStr, arbitrary[Mode]) {
+            (authority, mode) =>
+              val answers = emptyUserAnswers.setValue(EndorsementAuthorityPage(incidentIndex), authority)
+
+              val helper = new IncidentAnswersHelper(answers, mode, incidentIndex)
+              val result = helper.endorsementAuthority.get
+
+              result.key.value mustBe "Authority"
+              result.value.value mustBe authority
+              val actions = result.actions.get.items
+              actions.size mustBe 1
+              val action = actions.head
+              action.content.value mustBe "Change"
+              action.href mustBe routes.EndorsementAuthorityController.onPageLoad(answers.mrn, mode, incidentIndex).url
+              action.visuallyHiddenText.get mustBe "endorsement authority"
+              action.id mustBe "change-endorsement-authority"
+          }
+        }
+      }
+    }
+
+    "endorsement-country" - {
+      "must return None" - {
+        "when EndorsementCountryPage is undefined" in {
+          forAll(arbitrary[Mode]) {
+            mode =>
+              val helper = new IncidentAnswersHelper(emptyUserAnswers, mode, incidentIndex)
+              val result = helper.endorsementCountry
+              result mustBe None
+          }
+        }
+      }
+
+      "must return Some(Row)" - {
+        "when EndorsementCountryPage defined" in {
+          forAll(arbitrary[Country], arbitrary[Mode]) {
+            (country, mode) =>
+              val answers = emptyUserAnswers.setValue(EndorsementCountryPage(incidentIndex), country)
+
+              val helper = new IncidentAnswersHelper(answers, mode, incidentIndex)
+              val result = helper.endorsementCountry.get
+
+              result.key.value mustBe "Country"
+              result.value.value mustBe country.toString
+              val actions = result.actions.get.items
+              actions.size mustBe 1
+              val action = actions.head
+              action.content.value mustBe "Change"
+              action.href mustBe routes.EndorsementCountryController.onPageLoad(answers.mrn, mode, incidentIndex).url
+              action.visuallyHiddenText.get mustBe "endorsement country"
+              action.id mustBe "change-endorsement-country"
+          }
+        }
+      }
+    }
+
+    "endorsement-location" - {
+      "must return None" - {
+        "when EndorsementLocationPage is undefined" in {
+          forAll(arbitrary[Mode]) {
+            mode =>
+              val helper = new IncidentAnswersHelper(emptyUserAnswers, mode, incidentIndex)
+              val result = helper.endorsementLocation
+              result mustBe None
+          }
+        }
+      }
+
+      "must return Some(Row)" - {
+        "when EndorsementLocationPage defined" in {
+          forAll(Gen.alphaNumStr, arbitrary[Mode]) {
+            (location, mode) =>
+              val answers = emptyUserAnswers.setValue(EndorsementLocationPage(incidentIndex), location)
+
+              val helper = new IncidentAnswersHelper(answers, mode, incidentIndex)
+              val result = helper.endorsementLocation.get
+
+              result.key.value mustBe "Location"
+              result.value.value mustBe location
+              val actions = result.actions.get.items
+              actions.size mustBe 1
+              val action = actions.head
+              action.content.value mustBe "Change"
+              action.href mustBe routes.EndorsementLocationController.onPageLoad(answers.mrn, mode, incidentIndex).url
+              action.visuallyHiddenText.get mustBe "endorsement location"
+              action.id mustBe "change-endorsement-location"
+          }
+        }
+      }
+    }
+
+  }
+}
