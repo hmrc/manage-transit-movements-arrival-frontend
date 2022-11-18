e81fd7fe03f641be51558ed4ea112c73f02d45bd
doug.maxwell <douglasj.maxwell@icloud.com>
2022-11-18 09:29:43 +0000

    CTCP-1622: Declaration Goods Item Number

---
+++ app/controllers/incident/equipment/itemNumber/ItemNumberController.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.incident.equipment.itemNumber
+
+import controllers.actions._
+import controllers.{NavigatorOps, SettableOps, SettableOpsRunner}
+import forms.ItemNumberFormProvider
+import models.{Index, Mode, MovementReferenceNumber}
+import navigation.{ItemNumberNavigatorProvider, UserAnswersNavigator}
+import pages.incident.equipment.itemNumber.ItemNumberPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, Call, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.incident.equipment.itemNumber.ItemNumberView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class ItemNumberController @Inject() (
+  override val messagesApi: MessagesApi,
+  implicit val sessionRepository: SessionRepository,
+  navigatorProvider: ItemNumberNavigatorProvider,
+  formProvider: ItemNumberFormProvider,
+  actions: Actions,
+  val controllerComponents: MessagesControllerComponents,
+  view: ItemNumberView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  private val form = formProvider("incident.equipment.itemNumber")
+
+  def onPageLoad(mrn: MovementReferenceNumber, mode: Mode, incidentIndex: Index, equipmentIndex: Index, itemNumberIndex: Index): Action[AnyContent] =
+    actions.requireData(mrn) {
+      implicit request =>
+        val preparedForm = request.userAnswers.get(ItemNumberPage(incidentIndex, equipmentIndex, itemNumberIndex)) match {
+          case None        => form
+          case Some(value) => form.fill(value)
+        }
+
+        Ok(view(preparedForm, mrn, mode, routes.ItemNumberController.onSubmit(mrn, mode, incidentIndex, equipmentIndex, itemNumberIndex)))
+    }
+
+  def onSubmit(mrn: MovementReferenceNumber, mode: Mode, incidentIndex: Index, equipmentIndex: Index, itemNumberIndex: Index): Action[AnyContent] = {
+    val submit = routes.ItemNumberController.onSubmit(mrn, mode, incidentIndex, equipmentIndex, itemNumberIndex)
+    actions.requireData(mrn).async {
+      implicit request =>
+        form
+          .bindFromRequest()
+          .fold(
+            formWithErrors => Future.successful(BadRequest(view(formWithErrors, mrn, mode, submit))),
+            value => {
+              implicit val navigator: UserAnswersNavigator = navigatorProvider(mode, incidentIndex, equipmentIndex, itemNumberIndex)
+              ItemNumberPage(incidentIndex, equipmentIndex, itemNumberIndex).writeToUserAnswers(value).writeToSession().navigate()
+            }
+          )
+    }
+  }
+}
---
+++ test/views/incident/equipment/itemNumber/ItemNumberViewSpec.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.incident.equipment.itemNumber
+
+import controllers.incident.equipment.itemNumber.routes
+import forms.ItemNumberFormProvider
+import models.NormalMode
+import play.api.data.Form
+import play.twirl.api.HtmlFormat
+import viewModels.InputSize
+import views.behaviours.InputTextViewBehaviours
+import views.html.incident.equipment.itemNumber.ItemNumberView
+import org.scalacheck.{Arbitrary, Gen}
+
+class ItemNumberViewSpec extends InputTextViewBehaviours[String] {
+
+  override val prefix: String = "incident.equipment.itemNumber"
+
+  val call = routes.ItemNumberController.onSubmit(mrn, NormalMode, incidentIndex, equipmentIndex, itemNumberIndex)
+
+  override def form: Form[String] = new ItemNumberFormProvider()(prefix)
+
+  override def applyView(form: Form[String]): HtmlFormat.Appendable =
+    injector.instanceOf[ItemNumberView].apply(form, mrn, NormalMode, call)(fakeRequest, messages)
+
+  implicit override val arbitraryT: Arbitrary[String] = Arbitrary(Gen.alphaStr)
+
+  behave like pageWithTitle()
+
+  behave like pageWithBackLink()
+
+  behave like pageWithHeading()
+
+  behave like pageWithoutHint()
+
+  behave like pageWithInputText(Some(InputSize.Width20))
+
+  behave like pageWithSubmitButton("Continue")
+}
