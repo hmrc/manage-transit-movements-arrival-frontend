8b4a1160a70ed5b3c95ac2022b58318074353561
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-11-23 13:22:40 +0000

    CTCP-1770: Added controller and view for incident answers.

---
+++ app/controllers/incident/CheckIncidentAnswersController.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.incident
+
+import com.google.inject.Inject
+import controllers.actions.Actions
+import models.{Index, Mode, MovementReferenceNumber}
+import navigation.IncidentsNavigatorProvider
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import viewModels.incident.IncidentAnswersViewModel.IncidentAnswersViewModelProvider
+import views.html.incident.CheckIncidentAnswersView
+
+class CheckIncidentAnswersController @Inject() (
+  override val messagesApi: MessagesApi,
+  navigatorProvider: IncidentsNavigatorProvider,
+  actions: Actions,
+  val controllerComponents: MessagesControllerComponents,
+  view: CheckIncidentAnswersView,
+  viewModelProvider: IncidentAnswersViewModelProvider
+) extends FrontendBaseController
+    with I18nSupport {
+
+  def onPageLoad(mrn: MovementReferenceNumber, mode: Mode, index: Index): Action[AnyContent] = actions.requireData(mrn) {
+    implicit request =>
+      val sections = viewModelProvider(request.userAnswers, index, mode).sections
+      Ok(view(mrn, mode, index, sections))
+  }
+
+  def onSubmit(mrn: MovementReferenceNumber, mode: Mode, index: Index): Action[AnyContent] = actions.requireData(mrn) {
+    implicit request =>
+      Redirect(navigatorProvider(mode).nextPage(request.userAnswers))
+  }
+
+}
