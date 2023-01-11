9a9e6610fe14520b7e3f8ee72431c27a728f408c
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2023-01-11 22:55:38 +0000

    CTCP-2230: Resolve bobby warning and update other dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
   import play.core.PlayVersion
 
   private val mongoVersion = "0.74.0"
-  private val bootstrapVersion = "7.11.0"
+  private val bootstrapVersion = "7.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
@@ -12,8 +12,8 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "play-conditional-form-mapping"    % "1.12.0-play-28",
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-28"       % bootstrapVersion,
     "uk.gov.hmrc"          %% "play-allowlist-filter"            % "1.1.0",
-    "org.typelevel"        %% "cats-core"                        % "2.8.0",
-    "uk.gov.hmrc"          %% "play-frontend-hmrc"               % "3.34.0-play-28",
+    "org.typelevel"        %% "cats-core"                        % "2.9.0",
+    "uk.gov.hmrc"          %% "play-frontend-hmrc"               % "6.0.0-play-28",
     "com.chuusai"          %% "shapeless"                        % "2.3.10"
   )
 
@@ -22,15 +22,15 @@ object AppDependencies {
     "org.scalatest"              %% "scalatest"                % "3.2.14",
     "uk.gov.hmrc"                %% "bootstrap-test-play-28"   % bootstrapVersion,
     "com.typesafe.play"          %% "play-test"                % PlayVersion.current,
-    "org.mockito"                 % "mockito-core"             % "4.8.0",
+    "org.mockito"                 % "mockito-core"             % "4.11.0",
     "org.scalatestplus"          %% "mockito-4-6"              % "3.2.14.0",
     "org.scalacheck"             %% "scalacheck"               % "1.17.0",
     "org.scalatestplus"          %% "scalacheck-1-17"          % "3.2.14.0",
-    "io.github.wolfendale"       %% "scalacheck-gen-regexp"    % "1.0.0",
+    "io.github.wolfendale"       %% "scalacheck-gen-regexp"    % "1.1.0",
     "org.pegdown"                 % "pegdown"                  % "1.6.0",
     "org.jsoup"                   % "jsoup"                    % "1.15.3",
     "com.github.tomakehurst"      % "wiremock-standalone"      % "2.27.2",
-    "com.vladsch.flexmark"        % "flexmark-all"             % "0.62.2"
+    "com.vladsch.flexmark"        % "flexmark-all"             % "0.64.0"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
--- test/views/behaviours/CoordinatesViewBehaviours.scala
+++ test/views/behaviours/CoordinatesViewBehaviours.scala
@@ -20,7 +20,7 @@ import models.Coordinates
 
 trait CoordinatesViewBehaviours extends QuestionViewBehaviours[Coordinates] {
 
-  val fields = Seq("latitude", "longitude")
+  val fields: Seq[String] = Seq("latitude", "longitude")
 
   def pageWithCoordinatesInput(): Unit =
     "page with a coordinates input" - {
@@ -39,23 +39,9 @@ trait CoordinatesViewBehaviours extends QuestionViewBehaviours[Coordinates] {
           }
         }
 
-        "must not render an error summary" in {
-          assertNotRenderedById(doc, "error-summary-title")
-        }
+        behave like pageWithoutErrorSummary()
       }
 
-      for (field <- fields)
-        s"when rendered with an error for field '$field'" - {
-
-          "must show an error summary" in {
-            assertRenderedById(docWithError(field), "error-summary-title")
-          }
-
-          s"must show an error in the label for field '$field'" in {
-            val formGroupError = getElementByClass(docWithError(field), "govuk-form-group--error")
-            formGroupError.getElementsByClass("govuk-label").first().attr("for") mustBe field
-            formGroupError.getElementsByClass("govuk-error-message").first().id() mustBe s"$field-error"
-          }
-        }
+      behave like pageWithFieldErrorSummary(fields)
     }
 }
