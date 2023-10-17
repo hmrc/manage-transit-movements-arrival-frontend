2777a80ac19cbf7569661b15651c21d863aefe5c
Jake Goodwin <12535866+JakeGoodwin@users.noreply.github.com>
2023-10-17 08:54:18 +0100

    Updated beta banner feedback link

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -18,25 +18,17 @@ package config
 
 import com.google.inject.{Inject, Singleton}
 import play.api.Configuration
-import play.api.mvc.RequestHeader
-import uk.gov.hmrc.http.StringContextOps
 
 import java.time.LocalDate
 
 @Singleton
 class FrontendAppConfig @Inject() (configuration: Configuration) {
 
-  val contactHost: String                  = configuration.get[String]("contact-frontend.host")
-  val contactFormServiceIdentifier: String = "CTCTraders"
+  val contactHost: String = configuration.get[String]("contact-frontend.host")
 
   val showPhaseBanner: Boolean        = configuration.get[Boolean]("banners.showPhase")
   val userResearchUrl: String         = configuration.get[String]("urls.userResearch")
   val showUserResearchBanner: Boolean = configuration.get[Boolean]("banners.showUserResearch")
-
-  private val host: String = configuration.get[String]("host")
-
-  def feedbackUrl(implicit request: RequestHeader): String =
-    s"$contactHost/contact/beta-feedback?service=$contactFormServiceIdentifier&backUrl=${url"${host + request.uri}"}"
 
   val signOutUrl: String          = configuration.get[String]("urls.logoutContinue") + configuration.get[String]("urls.feedback")
   val loginHmrcServiceUrl: String = configuration.get[String]("urls.loginHmrcService")
