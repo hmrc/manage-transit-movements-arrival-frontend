c5360b056e1d34fd82fbef412961cc243eb6fa13
kenneth-rayner <kenneth.rayner@digital.hmrc.gov.uk>
2024-06-20 10:45:20 +0100

    updateDep: update app dependencies and plugins to latest versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,16 +1,15 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
-  import play.core.PlayVersion
 
-  private val bootstrapVersion = "8.3.0"
+  private val bootstrapVersion = "9.0.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"          %% "play-conditional-form-mapping-play-30"    % "2.0.0",
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-30"               % bootstrapVersion,
     "org.typelevel"        %% "cats-core"                                % "2.10.0",
-    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"               % "9.11.0",
+    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"               % "10.1.0",
     "com.chuusai"          %% "shapeless"                                % "2.3.12"
   )
 
