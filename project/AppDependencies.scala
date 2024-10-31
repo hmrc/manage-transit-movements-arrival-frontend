1b3be75450846c13012b124baa6d901ff96672d9
Okeremeta Jude Tega <31142281+justega247@users.noreply.github.com>
2024-10-31 15:05:57 +0000

    CTCP-5989: Update play-frontend dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,7 +9,7 @@ object AppDependencies {
     "uk.gov.hmrc"          %% "play-conditional-form-mapping-play-30"    % "3.2.0",
     "uk.gov.hmrc"          %% "bootstrap-frontend-play-30"               % bootstrapVersion,
     "org.typelevel"        %% "cats-core"                                % "2.12.0",
-    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"               % "10.13.0"
+    "uk.gov.hmrc"          %% "play-frontend-hmrc-play-30"               % "11.2.0"
   )
 
   val test: Seq[ModuleID] = Seq(
