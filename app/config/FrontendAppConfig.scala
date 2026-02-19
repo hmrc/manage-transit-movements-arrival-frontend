24de306a92cc4820fd4317f1847ed59497d39181
damien.butterworth <damienbutterworth@hotmail.co.uk>
2026-02-19 09:19:07 +0000

    Add the ability to disable UnLocode codelist lookup

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -30,6 +30,8 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
 
   val apiVersion: String = if (phase6Enabled) "2.0" else "1.0"
 
+  lazy val disableUnLocodeExtendedLookup: Boolean = configuration.get[Boolean]("disable-un-locode-extended-lookup")
+
   lazy val countriesOfDestination: Seq[String] = configuration.get[Seq[String]]("countriesOfDestination")
 
   val contactHost: String = configuration.get[String]("contact-frontend.host")
